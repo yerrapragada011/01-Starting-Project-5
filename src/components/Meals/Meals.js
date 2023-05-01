65b5be1b638aa2d8d96df2e157e8e8775a32e315
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-05-01 15:40:53 -0400

    Adding a Meals Component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+import MealsSummary from './MealsSummary';
+import AvaliableMeals from './AvaliableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvaliableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
